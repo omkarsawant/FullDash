4c3c434262a9b8c8aaca7a33af35c9f5cc027f17
Sawant <Omkar.Sawant@aig.com>
2020-01-08 17:25:05 -0500

    Router fields updated

--- routerApp/models.py
+++ routerApp/models.py
@@ -2,5 +2,11 @@ from django.db import models
 
 
 class Router(models.Model):
+    INTERFACE_CHOICES = [
+        ('GigabitEthernet0/0/0', 'GigE0/0/0'),
+        ('GigabitEthernet0/0/1', 'GigE0/0/1'),
+    ]
     hostname = models.CharField(max_length=19)
     loopback = models.GenericIPAddressField(protocol='IPv4')
+    interface = models.CharField(
+        max_length=100, choices=INTERFACE_CHOICES, default='GigabitEthernet0/0/0')
