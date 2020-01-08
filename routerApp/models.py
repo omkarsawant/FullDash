fc3e105073911ab81bdcdf96b39be4de11a86323
Sawant <Omkar.Sawant@aig.com>
2020-01-08 11:57:23 -0500

    Router fields updated

--- routerApp/models.py
+++ routerApp/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 
 class Router(models.Model):
-    hostname = models.CharField(max_length=10)
+    hostname = models.CharField(max_length=19)
     loopback = models.GenericIPAddressField(protocol='IPv4')
